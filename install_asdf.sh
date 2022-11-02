877432d41994a01d6cb00b1efaeb6fb666208a7f
johnny-celavi <kovacevicnikola51@gmail.com>
2022-11-02 17:12:10 +0100

    wip

--- install_asdf.sh
+++ install_asdf.sh
@@ -1,19 +1,11 @@
 #! /bin/bash
 
-echo Updating
-
-sudo apt update --yes
-
-echo Upgrading
-
-sudo apt upgrade --yes
-
 echo Cloning asdf...
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 echo Adding asdf to bashrc
 
-echo . $HOME/.asdf/completions/asdf.bash >> .bashrc
+echo . $HOME/.asdf/completions/asdf.bash >> ~/.bashrc
 
-echo . $HOME/.asdf/asdf.sh >> .bashrc
+echo . $HOME/.asdf/asdf.sh >> ~/.bashrc
