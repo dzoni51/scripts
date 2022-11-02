a2c211f7870b6eb9c51619fbf64141ea631038e6
johnny-celavi <kovacevicnikola51@gmail.com>
2022-11-02 17:43:42 +0100

    wip

--- install_erlang_elixir.sh
+++ install_erlang_elixir.sh
@@ -8,6 +8,26 @@ echo Adding Elixir asdf plugin
 
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 
+echo Making sure required packages are installed
+
+sudo apt-get install make
+
+sudo apt-get install automake
+
+sudo apt-get install libssl-dev
+
+sudo apt-get install autoconf
+
+sudo apt-get install libncurses5-dev
+
+sudo apt-get install gcc
+
+sudo apt-get install unzip
+
 asdf install erlang 25.1.1 --yes
 
 asdf install elixir 1.14.0 --yes
+
+asdf global elixir 1.14.0
+
+asdf global erlang 25.1.1
